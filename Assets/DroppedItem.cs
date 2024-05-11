16008f2196b28a6052dcacde9f6f7e64a6314963
Igor Ityanagui de Andrade <igorityanagui@hotmail.com>
2024-05-10 23:42:20 -0300

    Mapa v17
    Ajuste do drop de item

--- Assets/DroppedItem.cs
+++ Assets/DroppedItem.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class DroppedItem : MonoBehaviour, IInteractable2
 {
-    public float floatSpeed = 1f; // Velocidade de flutuação do item
-    public float floatHeight = 0.5f; // Altura de flutuação do item
-    public float rotationSpeed = 90f; // Velocidade de rotação do item
+    public float minHeight = 1.25f; // Altura mínima 
+    public float maxHeight = 1.5f; // Altura máxima 
+    public float floatSpeed = 1f; // Velocidade 
+    public float rotationSpeed = 90f; // Velocidade de rotação
 
     private void Update()
     {
-        // Faz o item flutuar
-        transform.position = new Vector3(transform.position.x, Mathf.Sin(Time.time * floatSpeed) * floatHeight, transform.position.z);
+        // Usa a função seno para calcular a flutuação
+        float yPos = Mathf.Sin(Time.time * floatSpeed) * (maxHeight - minHeight) * 0.5f + (maxHeight + minHeight) * 0.5f;
 
-        // Rotaciona o item
+        // Aplica a flutuação obtida
+        Vector3 newPosition = transform.position;
+        newPosition.y = yPos;
+        transform.position = newPosition;
+
+        // Rotaciona
         transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
     }
 
