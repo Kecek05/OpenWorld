8feb07ca513a9f0631fd0dc6068a36aa0f4dc74e
Igor Ityanagui de Andrade <igorityanagui@hotmail.com>
2024-05-17 22:52:05 -0300

    v5
    Pedras ao redor do rio, ajustes no que necessario e abobora na vila 2, como um teste

--- Assets/Scripts/IA/EnemyAnimatorController.cs
+++ Assets/Scripts/IA/EnemyAnimatorController.cs
@@ -10,13 +10,11 @@ public class EnemyAnimatorController : MonoBehaviour
     public void SetSpeed(float speed)
     {
         animator.SetFloat("Speed", speed);
-        Debug.Log("Speed set to: " + speed);
     }
 
     public void SetTurn(float turn)
     {
         animator.SetFloat("Turn", turn);
-        Debug.Log("Turn set to: " + turn);
     }
 
 
