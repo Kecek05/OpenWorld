63807117ef4d6980e606c4f16a451ddec735495b
SaLeVs <141702002+SaLeVs@users.noreply.github.com>
2024-05-24 02:31:36 -0300

    v3
    Random Itens + player new Move + itensCollect

--- Assets/Scripts/Greenhouse/ItemOnGround.cs
+++ Assets/Scripts/Greenhouse/ItemOnGround.cs
@@ -17,7 +17,7 @@ public class ItemOnGround : MonoBehaviour, IInteractable
     {
         int randomItemOnGround = Random.Range(0, itemOnGroundSOArray.Length);
         selectedItemOnGroundSO = itemOnGroundSOArray[randomItemOnGround];
-
+        
         selectedItemClicksToCollect = selectedItemOnGroundSO.clicksToCollect;
 
         itemInGround = Instantiate(selectedItemOnGroundSO.prefab, transform.position, Quaternion.identity);
@@ -27,16 +27,45 @@ public class ItemOnGround : MonoBehaviour, IInteractable
     public void Interact()
     {
         selectedItemClicksToCollect--;
-        Debug.Log(selectedItemClicksToCollect);
+
         if(selectedItemClicksToCollect <= 0 )
         {
             switch(selectedItemOnGroundSO.itemType)
             {
+                case PlayerItens.ItensType.Bread:
+                   PlayerItens.main.SetBreadCount(PlayerItens.main.GetBreadCount() + 1);
+                   break;
+                case PlayerItens.ItensType.MeatPatty:
+                   PlayerItens.main.SetMeatyPattyCount(PlayerItens.main.GetMeatyPattyCount() + 1);
+                   break;
+                case PlayerItens.ItensType.Chesse:
+                   PlayerItens.main.SetCheeseCount(PlayerItens.main.GetCheeseCount() + 1);
+                   break;
+                case PlayerItens.ItensType.Cabbage:
+                   PlayerItens.main.SetCabbageCount(PlayerItens.main.GetCabbageCount() + 1);
+                   break;
                 case PlayerItens.ItensType.Carambola:
-                    PlayerItens.main.SetCarambolaCount(PlayerItens.main.GetCarambolaCount() + 1);
+                   PlayerItens.main.SetCarambolaCount(PlayerItens.main.GetCarambolaCount() + 1);
+                   break;
+                case PlayerItens.ItensType.Tomato:
+                   PlayerItens.main.SetTomatoCount(PlayerItens.main.GetTomatoCount() + 1);
+                   break;
+                case PlayerItens.ItensType.Cogumelo:
+                   PlayerItens.main.SetCogumeloCount(PlayerItens.main.GetCogumeloCount() + 1);
+                   break;
+                case PlayerItens.ItensType.Flor:
+                   PlayerItens.main.SetFlorCount(PlayerItens.main.GetFlorCount() + 1);
+                   break;
+                case PlayerItens.ItensType.Lavanda:
+                   PlayerItens.main.SetLavandaCount(PlayerItens.main.GetLavandaCount() + 1);
+                   break;
+                case PlayerItens.ItensType.Mandragora:
+                   PlayerItens.main.SetMandragoraCount(PlayerItens.main.GetMandragoraCount() + 1);
+                   break;
+                case PlayerItens.ItensType.Samambaia:
+                    PlayerItens.main.SetSamambaiaCount(PlayerItens.main.GetSamambaiaCount() + 1);
                     break;
             }
-
 
             Destroy(itemInGround.gameObject);
             Destroy(gameObject);
--- Assets/Scripts/PlayerItens.cs
+++ Assets/Scripts/PlayerItens.cs
@@ -6,12 +6,31 @@ public class PlayerItens : MonoBehaviour
 {
     public static PlayerItens main;
 
+    private int breadCount;
+    private int meatPattyCount;
+    private int cheeseCount;
+    private int cabbageCount;
     private int carambolaCount;
+    private int tomatoCount;
+    private int cogumeloCount;
+    private int florCount;
+    private int lavandaCount;
+    private int mandragoraCount;
+    private int samambaiaCount;
 
     public enum ItensType
     {
+        Bread,
+        MeatPatty,
+        Chesse,
+        Cabbage,
         Carambola,
-
+        Tomato,
+        Cogumelo,
+        Flor,
+        Lavanda,
+        Mandragora,
+        Samambaia,
     }
 
 
@@ -22,8 +41,47 @@ public class PlayerItens : MonoBehaviour
     }
 
 
+    public void SetBreadCount(int _breadCount) { breadCount = _breadCount; }
+    public int GetBreadCount() { return breadCount; }
+
+
+    public void SetMeatyPattyCount(int _meatyPatty) { meatPattyCount = _meatyPatty; }
+    public int GetMeatyPattyCount() { return meatPattyCount; }
+
+
+    public void SetCheeseCount(int _cheeseCount) { cheeseCount = _cheeseCount; }
+    public int GetCheeseCount() { return cheeseCount; }
+
+
+    public void SetCabbageCount(int _cabbageCount) { cabbageCount = _cabbageCount; }
+    public int GetCabbageCount() { return cabbageCount; }
+
 
     public void SetCarambolaCount(int _carambolaCount) { carambolaCount = _carambolaCount; }
-
     public int GetCarambolaCount() {  return carambolaCount; }
+
+
+    public void SetTomatoCount(int _tomatoCount) { tomatoCount = _tomatoCount; }
+    public int GetTomatoCount() {  return tomatoCount; }
+
+
+    public void SetCogumeloCount(int _cogumeloCount) { cogumeloCount = _cogumeloCount; }
+    public int GetCogumeloCount() { return cogumeloCount; }
+
+
+    public void SetFlorCount(int _florCount) {  florCount = _florCount; }
+    public int GetFlorCount() {  return florCount; }
+
+
+    public void SetLavandaCount(int _lavandaCount) { lavandaCount = _lavandaCount; }
+    public int GetLavandaCount() {  return lavandaCount; }
+
+
+    public void SetMandragoraCount(int _mangragoraCount) { mandragoraCount = _mangragoraCount; }
+    public int GetMandragoraCount() {  return mandragoraCount; }
+
+
+    public void SetSamambaiaCount(int _samambaiaCount) { samambaiaCount = _samambaiaCount; }
+    public int GetSamambaiaCount() { return samambaiaCount; }
+
 }
