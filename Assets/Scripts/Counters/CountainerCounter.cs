b2c9a8d942e89ad80ceb5797b659747f7e640c35
João Felipe <jfk.abrao@hotmail.com>
2024-06-03 16:32:10 -0300

    Itens Limit

--- Assets/Scripts/Counters/CountainerCounter.cs
+++ Assets/Scripts/Counters/CountainerCounter.cs
@@ -11,15 +11,30 @@ public class CountainerCounter : BaseCounter
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+    [SerializeField] private PlayerItens.ItensType CounterType;
+
     public override void Interact(Player player)
     {
-        if(!player.HasKitchenObject())
+        if(!player.HasKitchenObject() && PlayerItens.Instance.TrySpawnItem(CounterType))
         {
-            //player is not carrying anything
+            //player is not carrying anything and can grab one more kitchenObject
             KitchenObject.SpawnKitchenObject(kitchenObjectSO, player);
 
             OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
 
+        } else if (player.HasKitchenObject())
+        {
+            //player is carrying something
+            if(player.GetKitchenObject().GetKitchenObjectSO() == kitchenObjectSO)
+            {
+                //player is carrying same kitchenObject of the Countainer
+
+                //Return the item
+                PlayerItens.Instance.ReturnItemToCountainer(CounterType);
+
+                //Destroy the item
+                player.GetKitchenObject().DestroySelf();
+            }
         }
     }
 }
--- Assets/Scripts/PlayerItens.cs
+++ Assets/Scripts/PlayerItens.cs
@@ -13,6 +13,8 @@ public class PlayerItens : MonoBehaviour
     private int mandragoraCount;
     private int samambaiaCount;
 
+    [SerializeField] private int[] itensCount;
+
     public enum ItensType
     {
         Carambola,
@@ -30,29 +32,102 @@ public class PlayerItens : MonoBehaviour
         DontDestroyOnLoad(Instance);
     }
 
+    public bool TrySpawnItem(ItensType itemTypeToSpawn)
+    {
+        switch (itemTypeToSpawn)
+        {
+            case ItensType.Carambola:
+                if (itensCount[0] > 0)
+                {
+                    itensCount[0]--;
+                    return true;
+                }
+                break;
+            case ItensType.Cogumelo:
+                if (itensCount[1] > 0)
+                {
+                    itensCount[1]--;
+                    return true;
+                }
+                break;
+            case ItensType.Flor:
+                if (itensCount[2] > 0)
+                {
+                    itensCount[2]--;
+                    return true;
+                }
+                break;
+            case ItensType.Lavanda:
+                if (itensCount[3] > 0)
+                {
+                    itensCount[3]--;
+                    return true;
+                }
+                break;
+            case ItensType.Mandragora:
+                if (itensCount[4] > 0)
+                {
+                    itensCount[4]--;
+                    return true;
+                }
+                break;
+            case ItensType.Samambaia:
+                if (itensCount[5] > 0)
+                {
+                    itensCount[5]--;
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+
+    public void ReturnItemToCountainer(ItensType itemTypeToReturn)
+    {
+        switch (itemTypeToReturn)
+        {
+            case ItensType.Carambola:
+                    itensCount[0]++;
+                break;
+            case ItensType.Cogumelo:
+                    itensCount[1]++;
+                break;
+            case ItensType.Flor:
+                    itensCount[2]++;
+                break;
+            case ItensType.Lavanda:
+                    itensCount[3]++;
+                break;
+            case ItensType.Mandragora:
+                    itensCount[4]++;
+                break;
+            case ItensType.Samambaia:
+                    itensCount[5]++;
+                break;
+        }
+    }
+
+    public void SetCarambolaCount(int _carambolaCount) { itensCount[0] = _carambolaCount; }
+    public int GetCarambolaCount() {  return itensCount[0]; }
 
 
-    public void SetCarambolaCount(int _carambolaCount) { carambolaCount = _carambolaCount; }
-    public int GetCarambolaCount() {  return carambolaCount; }
+    public void SetCogumeloCount(int _cogumeloCount) { itensCount[1] = _cogumeloCount; }
+    public int GetCogumeloCount() { return itensCount[1]; }
 
 
-    public void SetCogumeloCount(int _cogumeloCount) { cogumeloCount = _cogumeloCount; }
-    public int GetCogumeloCount() { return cogumeloCount; }
+    public void SetFlorCount(int _florCount) { itensCount[2] = _florCount; }
+    public int GetFlorCount() {  return itensCount[2]; }
 
 
-    public void SetFlorCount(int _florCount) {  florCount = _florCount; }
-    public int GetFlorCount() {  return florCount; }
+    public void SetLavandaCount(int _lavandaCount) { itensCount[3] = _lavandaCount; }
+    public int GetLavandaCount() {  return itensCount[3]; }
 
 
-    public void SetLavandaCount(int _lavandaCount) { lavandaCount = _lavandaCount; }
-    public int GetLavandaCount() {  return lavandaCount; }
+    public void SetMandragoraCount(int _mangragoraCount) { itensCount[4] = _mangragoraCount; }
+    public int GetMandragoraCount() {  return itensCount[4]; }
 
 
-    public void SetMandragoraCount(int _mangragoraCount) { mandragoraCount = _mangragoraCount; }
-    public int GetMandragoraCount() {  return mandragoraCount; }
-
-
-    public void SetSamambaiaCount(int _samambaiaCount) { samambaiaCount = _samambaiaCount; }
-    public int GetSamambaiaCount() { return samambaiaCount; }
+    public void SetSamambaiaCount(int _samambaiaCount) { itensCount[5] = _samambaiaCount; }
+    public int GetSamambaiaCount() { return itensCount[5]; }
 
 }
