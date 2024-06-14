135c5fd5a5436ef75ede30568fad09d480b27263
SaLeVs <alvesanicetodasilvag@gmail.com>
2024-06-14 00:32:32 -0300

    v6
    Separating UI and logic

--- Assets/Scripts/PaymentScene/PaymentController.cs
+++ Assets/Scripts/PaymentScene/PaymentController.cs
@@ -1,14 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using static MoneyController;
 
 public class PaymentController : MonoBehaviour
 {
+    public static MoneyController Instance;
+
+
     private int totalEconomy;
     private int expanseFixed = 50;
-    //private int[] expanses;
+    private int payoff;
+    private int[] expanses;
+    [SerializeField] private bool paymentConcluded = false;
 
     [SerializeField] private TextMeshProUGUI[] expanseTxt;
     [SerializeField] private TextMeshProUGUI economyTxt;
@@ -16,31 +23,31 @@ public class PaymentController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI fixedExpanseTxt;
     [SerializeField] private TextMeshProUGUI payOffTxt;
 
-    [SerializeField] private bool paymentConcluded = false;
+
     [SerializeField] private Loader.Scene scene;
-    private int payoff;
+    
 
     private void Start()
     {
         MoneyController.Instance.SetTotalMoney(MoneyController.Instance.GetTotalMoney() + MoneyController.Instance.GetDayMoney());
         totalEconomy = MoneyController.Instance.GetTotalMoney();
-        economyTxt.text = totalEconomy.ToString();
-        dayPaymentTxt.text = MoneyController.Instance.GetDayMoney().ToString();
-        fixedExpanseTxt.text = expanseFixed.ToString();
+        economyTxt.text = totalEconomy.ToString(); // passa para o texto, evento aqui
+        dayPaymentTxt.text = MoneyController.Instance.GetDayMoney().ToString(); // evento, pega o valor do dia e passar para o texto
+        fixedExpanseTxt.text = expanseFixed.ToString(); // evento, só passa o valor fixo
         
     }
 
 
     public void OnButtonClick()
     {
-       DoPayment();
+       DoPayment(); // evento para quando aperta o botão
     }
 
     void DoPayment()
     {
-        payoff = totalEconomy - expanseFixed;
-        payOffTxt.text = payoff.ToString();
-        paymentConcluded = true;
+        payoff = totalEconomy - expanseFixed; 
+        payOffTxt.text = payoff.ToString(); // passa o evento
+        paymentConcluded = true; // pagamento concluido
     }
 
    
---
+++ Assets/Scripts/PaymentScene/PaymentUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PaymentUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI[] expanseTxt;
+    [SerializeField] private TextMeshProUGUI economyTxt;
+    [SerializeField] private TextMeshProUGUI dayPaymentTxt;
+    [SerializeField] private TextMeshProUGUI fixedExpanseTxt;
+    [SerializeField] private TextMeshProUGUI payOffTxt;
+
+
+
+}
