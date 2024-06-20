63ad8e1f1b730c6d50817c53797aaf77616f7258
João Felipe <jfk.abrao@hotmail.com>
2024-06-20 14:58:46 -0300

    v2

--- Assets/Animation/Bruxinha Fix/MovimentoRosto.cs
+++ Assets/Animation/Bruxinha Fix/MovimentoRosto.cs
@@ -7,11 +7,6 @@ public class MovimentoRosto : MonoBehaviour
 
     public GameObject Rosto;
     public int Expressao;
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
 
     // Update is called once per frame
     void Update()
